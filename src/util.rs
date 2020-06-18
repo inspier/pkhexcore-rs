945f72bf65bf926666218b2853d14b2a75a0ebca
Devin <devdevin541@gmail.com>
2020-06-18 14:43:15 -0500

    bitconverter: Rewrote to use functions provided by std. Added extra docs and tests.

--- src/util.rs
+++ src/util.rs
@@ -1,1 +1,2 @@
+/// A module that converts base data types to an array of bytes, and an array of bytes to base data types.
 pub mod bitconverter;
--- src/util/bitconverter.rs
+++ src/util/bitconverter.rs
@@ -1,8 +1,10 @@
-use structure::*;
-
-/// A module that converts base data types to an array of bytes, and an array of bytes to base data types.
+use std::convert::TryInto;
 
 /// Returns a 16-bit unsigned integer converted from two bytes at a specified position in a byte array.
+///
+/// The `to_uint16` function converts the bytes from index start_index to start_index + 1
+/// to a `u16` value. The order of bytes in the array must reflect the endianness of the
+/// computer system's architecture.
 /// # Example
 ///
 /// ```
@@ -12,14 +14,19 @@ use structure::*;
 /// ```
 ///
 pub fn to_uint16(data: &[u8], start_index: usize) -> u16 {
-    let unpacker = structure!("<H");
-    let (result,) = unpacker
-        .unpack(&data[start_index..start_index + 2])
-        .expect("Failed to read uint16.");
-    return result;
+    let result = u16::from_le_bytes(
+        data[start_index..start_index + 2]
+            .try_into()
+            .expect("Failed to read u16. Invalid buffer provided."),
+    );
+    result
 }
 
 /// Returns a 32-bit unsigned integer converted from four bytes at a specified position in a byte array.
+///
+/// The `to_uint32` function converts the bytes from index start_index to start_index + 3
+/// to a `u32` value. The order of bytes in the array must reflect the endianness of the
+/// computer system's architecture.
 /// # Example
 ///
 /// ```
@@ -29,14 +36,19 @@ pub fn to_uint16(data: &[u8], start_index: usize) -> u16 {
 /// ```
 ///
 pub fn to_uint32(data: &[u8], start_index: usize) -> u32 {
-    let unpacker = structure!("<I");
-    let (result,) = unpacker
-        .unpack(&data[start_index..start_index + 4])
-        .expect("Failed to read uint32.");
-    return result;
+    let result = u32::from_le_bytes(
+        data[start_index..start_index + 4]
+            .try_into()
+            .expect("Failed to read u32. Invalid buffer provided."),
+    );
+    result
 }
 
 /// Returns a 64-bit unsigned integer converted from eight bytes at a specified position in a byte array.
+///
+/// The `to_uint64` function converts the bytes from index start_index to start_index + 7
+/// to a `u64` value. The order of bytes in the array must reflect the endianness of the
+/// computer system's architecture.
 /// # Example
 ///
 /// ```
@@ -46,11 +58,12 @@ pub fn to_uint32(data: &[u8], start_index: usize) -> u32 {
 /// ```
 ///
 pub fn to_uint64(data: &[u8], start_index: usize) -> u64 {
-    let unpacker = structure!("<Q");
-    let (result,) = unpacker
-        .unpack(&data[start_index..start_index + 8])
-        .expect("Failed to read uint64.");
-    return result;
+    let result = u64::from_le_bytes(
+        data[start_index..start_index + 8]
+            .try_into()
+            .expect("Failed to read u64. Invalid buffer provided."),
+    );
+    result
 }
 
 #[cfg(test)]
@@ -58,6 +71,7 @@ mod test {
     use super::*;
 
     #[test]
+    #[should_panic]
     fn to_uint16_test() {
         let buffer = vec![15, 0, 0, 255, 3, 16, 39, 255, 255, 127];
         assert_eq!(15, to_uint16(&buffer, 0));
@@ -66,9 +80,11 @@ mod test {
         assert_eq!(10000, to_uint16(&buffer, 5));
         assert_eq!(32767, to_uint16(&buffer, 8));
         assert_eq!(65535, to_uint16(&buffer, 7));
+        to_uint16(&buffer, 9);
     }
 
     #[test]
+    #[should_panic]
     fn to_uint32_test() {
         let buffer = vec![
             15, 0, 0, 0, 0, 16, 0, 255, 3, 0, 0, 202, 154, 59, 255, 255, 255, 255, 127,
@@ -80,9 +96,11 @@ mod test {
         assert_eq!(1000000000, to_uint32(&buffer, 10));
         assert_eq!(4294967295, to_uint32(&buffer, 14));
         assert_eq!(2147483647, to_uint32(&buffer, 15));
+        to_uint32(&buffer, 16);
     }
 
     #[test]
+    #[should_panic]
     fn to_uint64_test() {
         let buffer = vec![
             255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 100, 167, 179, 182, 224, 13, 0, 202,
@@ -98,5 +116,6 @@ mod test {
         assert_eq!(10000000000000000000, to_uint64(&buffer, 35));
         assert_eq!(18446744073709551615, to_uint64(&buffer, 43));
         assert_eq!(9223372036854775807, to_uint64(&buffer, 44));
+        to_uint64(&buffer, 45);
     }
 }
