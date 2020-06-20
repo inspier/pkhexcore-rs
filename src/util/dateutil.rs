3d0c8969e327e7859ee6ccfeaf84d00d99944d21
inspier <63922661+inspier@users.noreply.github.com>
2020-06-20 15:49:09 -0500

    test: Revised tests

--- src/util/dateutil.rs
+++ src/util/dateutil.rs
@@ -37,13 +37,10 @@ pub fn is_date_valid(year: u32, month: u32, day: u32) -> bool {
 /// ```
 /// use pkhexcore::util::dateutil::get_seconds_since2000;
 /// use chrono::{TimeZone, Utc};
-/// use std::time::Duration;
+///
 /// // (2000/1/2)  (2000,1,1)
 /// // 946771200 - 946684800 = 86,400 a full day in secs
-/// assert_eq!(
-///     get_seconds_since2000(Utc.ymd(2000, 1, 2)),
-///     Duration::from_secs(24 * 3600).as_secs(),
-/// );
+/// assert_eq!(86400, get_seconds_since2000(Utc.ymd(2000, 1, 2)));
 /// ```
 ///
 pub fn get_seconds_since2000(date: Date<Utc>) -> u64 {
@@ -53,204 +50,82 @@ pub fn get_seconds_since2000(date: Date<Utc>) -> u64 {
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::time::Duration;
 
-    struct TestCase {
-        year: u32,
-        month: u32,
-        day: u32,
-        expected: bool,
+    #[test]
+    fn recognizes_correct_dates() {
+        assert_eq!(true, is_date_valid(2000, 1, 1));
+        assert_eq!(true, is_date_valid(2001, 1, 31));
     }
 
     #[test]
-    fn test_valid_dates() {
-        let cases = [
-            TestCase {
-                year: 2000,
-                month: 1,
-                day: 1,
-                expected: true,
-            },
-            TestCase {
-                year: 2001,
-                month: 1,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 1,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 2,
-                day: 28,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 3,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 4,
-                day: 30,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 5,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 6,
-                day: 30,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 7,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 8,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 9,
-                day: 30,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 10,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 11,
-                day: 30,
-                expected: true,
-            },
-            TestCase {
-                year: 2016,
-                month: 12,
-                day: 31,
-                expected: true,
-            },
-            TestCase {
-                year: 0,
-                month: 0,
-                day: 0,
-                expected: false,
-            },
-            TestCase {
-                year: 2000,
-                month: 1,
-                day: 32,
-                expected: false,
-            },
-            TestCase {
-                year: 2000,
-                month: 13,
-                day: 1,
-                expected: false,
-            },
-            TestCase {
-                year: 10000,
-                month: 1,
-                day: 1,
-                expected: false,
-            },
-            TestCase {
-                year: 2000,
-                month: 1,
-                day: 0,
-                expected: false,
-            },
-            TestCase {
-                year: 2000,
-                month: 0,
-                day: 1,
-                expected: false,
-            },
-            TestCase {
-                year: 0,
-                month: 1,
-                day: 1,
-                expected: false,
-            },
-        ];
-
-        for case in cases.iter() {
-            assert_eq!(
-                case.expected,
-                is_date_valid(case.year, case.month, case.day),
-                "expected {}/{}/{} to be {}",
-                case.year,
-                case.month,
-                case.day,
-                if case.expected { "valid" } else { "invalid" },
-            );
-        }
+    fn recognizes_valid_month_boundaries() {
+        assert_eq!(true, is_date_valid(2016, 1, 31));
+        assert_eq!(true, is_date_valid(2016, 2, 28));
+        assert_eq!(true, is_date_valid(2016, 3, 31));
+        assert_eq!(true, is_date_valid(2016, 4, 30));
+        assert_eq!(true, is_date_valid(2016, 5, 31));
+        assert_eq!(true, is_date_valid(2016, 6, 30));
+        assert_eq!(true, is_date_valid(2016, 7, 31));
+        assert_eq!(true, is_date_valid(2016, 8, 31));
+        assert_eq!(true, is_date_valid(2016, 9, 30));
+        assert_eq!(true, is_date_valid(2016, 10, 31));
+        assert_eq!(true, is_date_valid(2016, 11, 30));
+        assert_eq!(true, is_date_valid(2016, 12, 31));
     }
 
     #[test]
-    fn test_recognizes_correct_leap_year() {
-        let cases = [
-            TestCase {
-                year: 2004,
-                month: 2,
-                day: 29,
-                expected: true,
-            },
-            TestCase {
-                year: 2005,
-                month: 2,
-                day: 29,
-                expected: false,
-            },
-        ];
-        for case in cases.iter() {
-            assert_eq!(
-                case.expected,
-                is_date_valid(case.year, case.month, case.day)
-            );
-        }
+    fn recognizes_correct_leap_year_test() {
+        assert_eq!(true, is_date_valid(2004, 2, 29));
+        assert_eq!(false, is_date_valid(2005, 2, 29));
     }
 
     #[test]
-    fn test_dates_since_epoch2000() {
-        // (2000/1/2) 946771200 - (2000,1,1) 946684800 = 86,400 a full day in
-        // secs
-        assert_eq!(
-            get_seconds_since2000(Utc.ymd(2000, 1, 2)),
-            Duration::from_secs(24 * 3600).as_secs(),
-        );
+    fn fails_with_zero_date() {
+        assert_eq!(false, is_date_valid(0, 0, 0));
+    }
 
-        // (2000/2/1) 949363200 - (2000/1/1) 946684800 = 31,622,400 a full month
-        // 31 days in jan
-        assert_eq!(
-            get_seconds_since2000(Utc.ymd(2000, 2, 1)),
-            Duration::from_secs(31 * 24 * 3600).as_secs(),
-        );
+    #[test]
+    fn fails_with_large_day() {
+        assert_eq!(false, is_date_valid(2000, 1, 32));
+    }
 
-        // (2001/1/1) 978307200 - (2000/1/1) 946684800 = 31,622,400 a full
-        // (leap) year in secs
-        assert_eq!(
-            get_seconds_since2000(Utc.ymd(2001, 1, 1)),
-            Duration::from_secs(366 * 24 * 3600).as_secs(),
-        );
+    #[test]
+    fn fails_with_large_month() {
+        assert_eq!(false, is_date_valid(2000, 13, 1));
+    }
+
+    #[test]
+    fn fails_with_large_year() {
+        assert_eq!(false, is_date_valid(10000, 1, 1));
+    }
+
+    #[test]
+    fn fails_with_zero_day() {
+        assert_eq!(false, is_date_valid(2000, 1, 0));
+    }
+
+    #[test]
+    fn fails_with_zero_month() {
+        assert_eq!(false, is_date_valid(2000, 0, 1));
+    }
+
+    #[test]
+    fn fails_with_zero_year() {
+        assert_eq!(false, is_date_valid(0, 1, 1));
+    }
+
+    #[test]
+    fn dates_since_epoch2000_test() {
+        // (2000/1/2)  (2000,1,1)
+        // 946771200 - 946684800 = 86,400 a full day in secs
+        assert_eq!(86400, get_seconds_since2000(Utc.ymd(2000, 1, 2)));
+
+        // (2000/2/1)  (2000/1/1)
+        // 949363200 - 946684800 = 31,622,400 a full month 31 days in Jan
+        assert_eq!(2678400, get_seconds_since2000(Utc.ymd(2000, 2, 1)));
+
+        // (2001/1/1)  (2000/1/1)
+        // 978307200 - 946684800 = 31,622,400 a full (leap) year in secs
+        assert_eq!(31622400, get_seconds_since2000(Utc.ymd(2001, 1, 1)));
     }
 }
