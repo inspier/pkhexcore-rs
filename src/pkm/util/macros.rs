3ddc2c765a9f96e190a25d4c774c52c98d2ea8a3
inspier <devdevin541@gmail.com>
2020-07-24 16:53:09 -0500

    pk8: Fixed broken Windows build.

--- src/pkm/util/macros.rs
+++ src/pkm/util/macros.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! get {
     ($self:ident, $field:ident) => {
-        paste::item! {
+        paste::expr! {
             $self.[<get_ $field:snake>]()
         }
     };
@@ -10,8 +10,26 @@ macro_rules! get {
 #[macro_export]
 macro_rules! set {
     ($self:ident, $field:ident, $value:expr) => {
-        paste::item! {
+        paste::expr! {
             $self.[<set_ $field:snake>]($value)
+        }
+    };
+}
+
+macro_rules! field {
+    ($self:ident; $field:ident; get: $out_type:ty => $getter:expr; set: $value_type:ty) => {
+        paste::item! {
+            #[logfn(INFO)]
+            #[logfn_inputs(Debug)]
+
+            pub fn [<$field:snake>](mut $self: Self, value: $value_type) -> Self {
+                $self.[<set_ $field:snake>](value);
+                $self
+            }
+
+            pub fn [<get_ $field:snake>]($self: &Self) -> $out_type {
+                $getter
+            }
         }
     };
 }
