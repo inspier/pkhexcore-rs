5d743d0e6e02508f2f8a9ee460d6af7e095534f9
inspier <devdevin541@gmail.com>
2020-07-24 21:12:30 -0500

    game: Removed unneeded paste block and fixed Windows build.

--- src/game/enums/util/macros.rs
+++ src/game/enums/util/macros.rs
@@ -17,8 +17,6 @@ macro_rules! impl_from {
 #[macro_use]
 macro_rules! string_to_enum {
     ($enum_name:tt, $value_name:tt) => {
-        paste::item! {
         $enum_name::$value_name
-        }
     };
 }
